9f688c6d863b1da5694c26be990adb44f3b22d0c
asif3062 <asifbsb2002@gmail.com>
2022-10-10 23:05:50 +0600

    options displayed on UI

--- src/Components/SingleQuestion/SingleQuestion.jsx
+++ src/Components/SingleQuestion/SingleQuestion.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const SingleQuestion = ({qst}) => {
-    const {question,correctAnswer,options} = qst;
+const SingleQuestion = ({ qst }) => {
+
+    const { question, correctAnswer, options } = qst;
     return (
-        <div className='bg-blue-200 p-3 rounded-md my-3'>
-            <h1 className=''>{question}</h1>
+        <div className='bg-blue-200 p-3 rounded-md my-3 md:mx-5'>
+            <div>
+                <h1 className='text-xl'>Question : {question} <small className='bg-green-400 p-1 rounded-sm'><button>Ans</button></small></h1>
+            </div>
+            <div className='flex justify-evenly items-center my-5'>
+                {
+                    options.map(opt => <button className='p-3 rounded-md bg-green-100 m-1'>{opt}</button>)
+                }
+            </div>
         </div>
     );
 };
