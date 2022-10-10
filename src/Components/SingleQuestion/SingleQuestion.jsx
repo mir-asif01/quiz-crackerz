32b0fe4e25879db917af59aad95ea686e894842c
asif3062 <asifbsb2002@gmail.com>
2022-10-10 23:17:58 +0600

    show/hide answer functionality added

--- src/Components/SingleQuestion/SingleQuestion.jsx
+++ src/Components/SingleQuestion/SingleQuestion.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SingleQuestion = ({ qst }) => {
 
     const { question, correctAnswer, options } = qst;
+    const [show,showAnswer] = useState(false);
 
     const handleChoiceBtn = (opt) =>{
         if(opt === correctAnswer ){
@@ -17,7 +18,12 @@ const SingleQuestion = ({ qst }) => {
     return (
         <div className='bg-blue-200 p-3 rounded-md my-3 md:mx-5'>
             <div>
-                <h1 className='text-xl'>Question : {question} <small className='bg-green-400 p-1 rounded-sm'><button>Ans</button></small></h1>
+                <h1 className='text-xl'>Question : {question} <small className='bg-green-400 p-1 rounded-sm'><button onClick={()=>showAnswer(!show)}>Ans</button></small></h1>
+            </div>
+            <div className='bg-amber-400'>
+                {
+                    show ? <h1>Answer : {correctAnswer}</h1> : ' '
+                }
             </div>
             <div className='flex justify-evenly items-center my-5'>
                 {
